d4a4b986da549c705946fe3ef614c8b73214a832
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-05 07:21:24 -0700

    Update test_icecream.py

--- test_icecream.py
+++ test_icecream.py
@@ -1,1 +1,8 @@
+import unittest
+from dessert import IceCream
 
+class TestIceCream(unittest.TestCase):
+
+    def test_calculate_cost(self):
+        icecream = IceCream("Vanilla", 2, 1.50)
+        self.assertEqual(icecream.calculate_cost(), 3.00)
