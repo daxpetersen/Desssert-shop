567db9e64f8a431bf9382e9717ad4b48413b53d7
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-05 07:20:05 -0700

    Update test_candy.py

--- test_candy.py
+++ test_candy.py
@@ -1,1 +1,9 @@
 
+import unittest
+from dessert import Candy
+
+class TestCandy(unittest.TestCase):
+
+    def test_calculate_cost(self):
+        candy = Candy("Candy Corn", 1.5, 0.50)
+        self.assertEqual(candy.calculate_cost(), 0.75)
