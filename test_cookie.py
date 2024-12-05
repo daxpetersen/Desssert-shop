e187155000343cdfd0ab2917cacce0a8c6dd138f
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-05 07:20:46 -0700

    Update test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -1,1 +1,8 @@
+import unittest
+from dessert import Cookie
 
+class TestCookie(unittest.TestCase):
+
+    def test_calculate_cost(self):
+        cookie = Cookie("Chocolate Chip", 12, 2.50)
+        self.assertEqual(cookie.calculate_cost(), 2.50)
