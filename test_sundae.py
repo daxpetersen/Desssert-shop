ab3625ad89cc6711b1f96f29085153003080cea8
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-05 07:22:26 -0700

    Update test_sundae.py

--- test_sundae.py
+++ test_sundae.py
@@ -1,1 +1,8 @@
+import unittest
+from dessert import Sundae
 
+class TestSundae(unittest.TestCase):
+
+    def test_calculate_cost(self):
+        sundae = Sundae("Chocolate", 2, 2.00, "Hot Fudge", 1.00)
+        self.assertEqual(sundae.calculate_cost(), 5.00)
