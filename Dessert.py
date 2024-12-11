146aae5964024056d8e90ff787cdac26df2482f6
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-11 07:28:48 -0700

    Update Dessert.py

--- Dessert.py
+++ Dessert.py
@@ -1,6 +1,17 @@
-class DessertItem:
+from abc import ABC, abstractmethod
+
+class DessertItem(ABC):
     def __init__(self, name):
         self.name = name
+        self.tax_percent = 7.25
+
+    @abstractmethod
+    def calculate_cost(self):
+        pass
+
+    def calculate_tax(self):
+        return self.calculate_cost() * (self.tax_percent / 100)
+
 
 class Candy(DessertItem):
     def __init__(self, name, weight, price_per_pound):
@@ -11,6 +22,7 @@ class Candy(DessertItem):
     def calculate_cost(self):
         return self.weight * self.price_per_pound
 
+
 class Cookie(DessertItem):
     def __init__(self, name, quantity, price_per_dozen):
         super().__init__(name)
@@ -19,6 +31,7 @@ class Cookie(DessertItem):
 
     def calculate_cost(self):
         return (self.quantity / 12) * self.price_per_dozen
+
 
 class IceCream(DessertItem):
     def __init__(self, name, scoops, price_per_scoop):
@@ -29,6 +42,7 @@ class IceCream(DessertItem):
     def calculate_cost(self):
         return self.scoops * self.price_per_scoop
 
+
 class Sundae(IceCream):
     def __init__(self, name, scoops, price_per_scoop, topping_name, topping_price):
         super().__init__(name, scoops, price_per_scoop)
@@ -38,6 +52,7 @@ class Sundae(IceCream):
     def calculate_cost(self):
         return super().calculate_cost() + self.topping_price
 
+
 class Order:
     def __init__(self):
         self.order = []
@@ -45,6 +60,13 @@ class Order:
     def add(self, dessert_item):
         self.order.append(dessert_item)
 
+    def order_cost(self):
+        return sum(item.calculate_cost() for item in self.order)
+
+    def order_tax(self):
+        return sum(item.calculate_tax() for item in self.order)
+
     def __len__(self):
         return len(self.order)
+
 
