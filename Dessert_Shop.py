4c916f0bdc8a44bbce3aadeed0f8ffae40a67f2a
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-11 07:28:10 -0700

    Update Dessert_Shop.py

--- Dessert_Shop.py
+++ Dessert_Shop.py
@@ -1,91 +1,55 @@
-from dessert import Candy, Cookie, IceCream, Sundae, Order
-
-def display_candy_menu():
-  print("\nAvailable Candies:")
-  print("1. Candy Corn")
-  print("2. Gummy Bears")
-  print("3. Chocolate")
-
-def display_cookie_menu():
-  print("\nAvailable Cookies:")
-  print("1. Chocolate Chip")
-  print("2. Oatmeal Raisin")
-  print("3. Peanut Butter")
-
-def display_ice_cream_menu():
-  print("\nAvailable Ice Cream Flavors:")
-  print("1. Vanilla")
-  print("2. Chocolate")
-  print("3. Pistachio")
-  print("4. Strawberry")
-
-def display_sundae_menu():
-  print("\nAvailable Sundae Flavors:")
-  print("1. Vanilla")
-  print("2. Chocolate")
-  print("3. Strawberry")
-
-def get_candy_order():
-  display_candy_menu()
-  name = input("Enter candy name: ")
-  weight = float(input("Enter amount of pounds: "))
-  price_per_pound = float(input("Enter price per pound: "))
-  return Candy(name, weight, price_per_pound)
-
-def get_cookie_order():
-  display_cookie_menu()
-  name = input("Enter cookie name: ")
-  quantity = int(input("Enter quantity: "))
-  price_per_dozen = float(input("Enter price per dozen: "))
-  return Cookie(name, quantity, price_per_dozen)
-
-def get_ice_cream_order():
-  display_ice_cream_menu()
-  name = input("Enter ice cream flavor: ")
-  scoops = int(input("Enter number of scoops: "))
-  price_per_scoop = float(input("Enter price per scoop: "))
-  return IceCream(name, scoops, price_per_scoop)
-
-def get_sundae_order():
-  display_sundae_menu()
-  name = input("Enter sundae flavor: ")
-  scoops = int(input("Enter number of scoops: "))
-  price_per_scoop = float(input("Enter price per scoop: "))
-  topping_name = input("Enter topping name: ")
-  topping_price = float(input("Enter topping price: "))
-  return Sundae(name, scoops, price_per_scoop, topping_name, topping_price)
+from Dessert import Candy, Cookie, IceCream, Sundae, Order
+from receipt import make_receipt
 
 def main():
-  order = Order()
+    order = Order()
 
-  while True:
-    print("\nChoose a dessert to add to your order:")
-    print("1. Candy")
-    print("2. Cookie")
-    print("3. Ice Cream")
-    print("4. Sundae")
-    print("5. Finish")
+    while True:
+        print("\nChoose a dessert to add to your order:")
+        print("1. Candy")
+        print("2. Cookie")
+        print("3. Ice Cream")
+        print("4. Sundae")
+        print("5. Finish")
 
-    choice = input("Enter your choice (1-5): ")
+        choice = input("Enter your choice (1-5): ")
 
-    if choice == '1':
-      order.add(get_candy_order())
-    elif choice == '2':
-      order.add(get_cookie_order())
-    elif choice == '3':
-      order.add(get_ice_cream_order())
-    elif choice == '4':
-      order.add(get_sundae_order())
-    elif choice == '5':
-      break
-    else:
-      print("Invalid choice, please try again.")
+        if choice == '1':
+            name = input("Enter candy name: ")
+            weight = float(input("Enter weight in pounds: "))
+            price_per_pound = float(input("Enter price per pound: "))
+            order.add(Candy(name, weight, price_per_pound))
+        elif choice == '2':
+            name = input("Enter cookie name: ")
+            quantity = int(input("Enter quantity: "))
+            price_per_dozen = float(input("Enter price per dozen: "))
+            order.add(Cookie(name, quantity, price_per_dozen))
+        elif choice == '3':
+            name = input("Enter ice cream flavor: ")
+            scoops = int(input("Enter number of scoops: "))
+            price_per_scoop = float(input("Enter price per scoop: "))
+            order.add(IceCream(name, scoops, price_per_scoop))
+        elif choice == '4':
+            name = input("Enter sundae flavor: ")
+            scoops = int(input("Enter number of scoops: "))
+            price_per_scoop = float(input("Enter price per scoop: "))
+            topping_name = input("Enter topping name: ")
+            topping_price = float(input("Enter topping price: "))
+            order.add(Sundae(name, scoops, price_per_scoop, topping_name, topping_price))
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice, please try again.")
 
-  print("\nYour order:")
-  for item in order.order:
-    print(item.name)
+    data = [["Name", "Item Cost", "Tax"]]
+    for item in order.order:
+        data.append([item.name, f"${item.calculate_cost():.2f}", f"${item.calculate_tax():.2f}"])
+    data.append(["Order Subtotals", f"${order.order_cost():.2f}", f"${order.order_tax():.2f}"])
+    data.append(["Order Total", "", f"${order.order_cost() + order.order_tax():.2f}"])
+    data.append(["Total items in the order", "", len(order)])
 
-  print("Total number of items in order:", len(order))
+    make_receipt(data, "receipt.pdf")
+    print("\nReceipt has been generated as 'receipt.pdf'.")
 
 if __name__ == "__main__":
-  main()
+    main()
