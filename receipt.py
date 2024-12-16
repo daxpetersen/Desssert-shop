e342b21a4411e7021dcd34ec9ec14abc94b13405
daxpetersen <dax.petersen@ucas-edu.net>
2024-12-16 07:32:25 -0700

    Update receipt.py

--- receipt.py
+++ receipt.py
@@ -2,16 +2,17 @@ from reportlab.lib.pagesizes import A4
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
 
-def make_receipt(data, out_file_name="receipt.pdf"):
-    pdf = SimpleDocTemplate(out_file_name, pagesize=A4)
+class make_receipt:
+    def build(data, out_file_name="receipt.pdf"):
+        pdf = SimpleDocTemplate(out_file_name, pagesize=A4)
 
-    table = Table(data)
-    style = TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.gray),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('GRID', (0, 0), (-1, -1), 1, colors.black)
-    ])
-    table.setStyle(style)
+        table = Table(data)
+        style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.gray),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ])
+        table.setStyle(style)
 
-    pdf.build([table])
+        pdf.build([table])
